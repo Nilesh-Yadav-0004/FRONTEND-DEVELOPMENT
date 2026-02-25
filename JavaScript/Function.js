bd81dc793dfb61f9db38ff64302f9caedfb0dc61
Nilesh Yadav <ny033222@gmail.com>
2026-02-25 21:12:45 +0530

    practice

---
+++ JavaScript/Function.js
@@ -0,0 +1,28 @@
+// //  Sub
+
+// function sub(a,b){
+//     return a-b;
+// }
+
+// console.log(sub(3,5))
+
+// console.log(sub(10,22))
+
+// function mult(a,b){
+//     return a*b;
+// }
+
+// console.log(mult(10,20))
+
+// function add(a,b,c){
+//     return a+b+c;
+// }
+
+// console.log(add(10,20,50))
+
+function div(a,b){
+    return a/b;
+}
+
+console.log(div(50,5))
+
